28b79cbf5af51e8c2bb22818f7df2ba5fb5ac76b
Piruin Panichphol <piruin.p@gmail.com>
2020-08-01 16:31:48 +0700

    copy user_permission from node_module to extension

---
+++ extensions/users-permissions/config/layout.js
@@ -0,0 +1,8 @@
+module.exports = {
+  user: {
+    actions: {
+      create: 'User.create', // Use the User plugin's controller.
+      update: 'User.update',
+    },
+  },
+};
---
+++ extensions/users-permissions/services/User.js
@@ -0,0 +1,111 @@
+'use strict';
+
+/**
+ * User.js service
+ *
+ * @description: A set of functions similar to controller's actions to avoid code duplication.
+ */
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  /**
+   * Promise to count users
+   *
+   * @return {Promise}
+   */
+
+  count(params) {
+    return strapi.query('user', 'users-permissions').count(params);
+  },
+
+  /**
+   * Promise to search count users
+   *
+   * @return {Promise}
+   */
+
+  countSearch(params) {
+    return strapi.query('user', 'users-permissions').countSearch(params);
+  },
+
+  /**
+   * Promise to add a/an user.
+   * @return {Promise}
+   */
+  async add(values) {
+    if (values.password) {
+      values.password = await strapi.plugins['users-permissions'].services.user.hashPassword(
+        values
+      );
+    }
+
+    return strapi.query('user', 'users-permissions').create(values);
+  },
+
+  /**
+   * Promise to edit a/an user.
+   * @return {Promise}
+   */
+  async edit(params, values) {
+    if (values.password) {
+      values.password = await strapi.plugins['users-permissions'].services.user.hashPassword(
+        values
+      );
+    }
+
+    return strapi.query('user', 'users-permissions').update(params, values);
+  },
+
+  /**
+   * Promise to fetch a/an user.
+   * @return {Promise}
+   */
+  fetch(params, populate = ['role']) {
+    return strapi.query('user', 'users-permissions').findOne(params, populate);
+  },
+
+  /**
+   * Promise to fetch all users.
+   * @return {Promise}
+   */
+  fetchAll(params, populate) {
+    return strapi.query('user', 'users-permissions').find(params, populate);
+  },
+
+  hashPassword(user = {}) {
+    return new Promise(resolve => {
+      if (!user.password || this.isHashed(user.password)) {
+        resolve(null);
+      } else {
+        bcrypt.hash(`${user.password}`, 10, (err, hash) => {
+          resolve(hash);
+        });
+      }
+    });
+  },
+
+  isHashed(password) {
+    if (typeof password !== 'string' || !password) {
+      return false;
+    }
+
+    return password.split('$').length === 4;
+  },
+
+  /**
+   * Promise to remove a/an user.
+   * @return {Promise}
+   */
+  async remove(params) {
+    return strapi.query('user', 'users-permissions').delete(params);
+  },
+
+  async removeAll(params) {
+    return strapi.query('user', 'users-permissions').delete(params);
+  },
+
+  validatePassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  },
+};
