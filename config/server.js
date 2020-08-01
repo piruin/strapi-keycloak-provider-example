b98dab653bf5cf159e28f127fcf44ce879cc935e
Piruin Panichphol <piruin.p@gmail.com>
2020-08-01 14:14:48 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da29409e9695893a668befd4403341c4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '02bb575a-bfa9-4c8c-aaca-33a752744fef'
+};
